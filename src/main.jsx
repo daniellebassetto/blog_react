2e4b6f2d07def7f6cc863f3ea8a9216cec35c44c
Danielle Bassetto <danibbassetto@hotmail.com>
2025-03-13 22:29:00 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+import Post from './routes/Post.jsx'
+import Admin from './routes/Admin.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/new",
+        element: <NewPost/>
+      },
+      {
+        path: "/posts/:id",
+        element: <Post/>
+      },
+      {
+        path: "/admin",
+        element: <Admin/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
---
+++ src/routes/Admin.jsx
@@ -0,0 +1,12 @@
+import blogFetch from "../axios/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './Admin.css'
+
+const Admin = () => {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default Admin
